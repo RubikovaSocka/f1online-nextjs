7d700d8ab64dd7860ec98722770b9e3aa302561e
RubikovaSocka <mincorbr@gmail.com>
2020-05-13 08:50:32 +0200

    desktop version full beta

--- pages/_app.js
+++ pages/_app.js
@@ -74,7 +74,7 @@ export default function App({ Component, pageProps }) {
     return (
         <>
             <Head>
-                <link rel="shortcut icon" href="./favicon.ico" />
+                <link rel="shortcut icon" href="./public/favicon.png" />
                 {/*<meta property="og:image" content={`${postData.better_featured_image.source_url}`} />*/}
             </Head>
             <div className={cstyles.cookieContainer}>
