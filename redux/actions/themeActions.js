6e97cb7f5c0e64aaabfe3fdaf6e301255b646e8c
milanku <kubala@post.com>
2020-10-19 00:10:29 +0200

    created actions for theme change

--- redux/actions/themeActions.js
+++ redux/actions/themeActions.js
@@ -1,14 +1,7 @@
-import { CHANGE_THEME } from "./types.js";
+import { THEME } from "../constants";
 
+const changeTheme = () => ({
+  type: THEME.CHANGE
+});
 
-
-export const changeTheme = (data) => (dispatch, state) => {
-  console.log("action: ")
-  console.log(data)
-  if (data.themeName) {
-    dispatch({
-      type: CHANGE_THEME,
-      payload: data.themeName
-    });
-  }
-};
+export { changeTheme };
