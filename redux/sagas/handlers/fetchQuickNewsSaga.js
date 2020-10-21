e0b9b6a8b341c3a6da4fc1977866ddd8426509b8
milanku <kubala@post.com>
2020-10-21 15:07:17 +0200

    created quicknews api calls with saga

--- redux/sagas/handlers/fetchQuickNewsSaga.js
+++ redux/sagas/handlers/fetchQuickNewsSaga.js
@@ -1,10 +1,10 @@
 import { call, put } from "redux-saga/effects";
 import fetchQuickNews from "../../../apis/fetchQuickNewsApi";
-import { setQuickNews, setQuickNewsError } from "../../actions/articlesActions";
+import { setQuickNews, setQuickNewsError } from "../../actions/quickNewsActions";
 
 function* fetchQuickNewsSaga() {
   try {
-    const quickNews = yield call(fetchArticles);
+    const quickNews = yield call(fetchQuickNews);
     yield put(setQuickNews(quickNews));
   } catch (err) {
     yield put(setQuickNewsError(err));
