0c7df77153a489bcf39924328390cf6022959ef2
milanku <kubala@post.com>
2020-10-22 00:37:23 +0200

    refactored Section Title component to functional

--- components/SectionTitle/SectionTitle.js
+++ components/SectionTitle/SectionTitle.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react'
-import styles from './SectionTitle.module.scss'
+import React from "react";
+import styles from "./SectionTitle.module.scss";
 
-export class SectionTitle extends Component {
-    render() {
-        return (
-            <div className={styles.titleContainer}>
-                <h2 className={styles.title}>{this.props.title}</h2>
-            </div>
-        )
-    }
+function SectionTitle({ title }) {
+  return (
+    <div className={styles.titleContainer}>
+      <h2 className={styles.title}>{title}</h2>
+    </div>
+  );
 }
 
-export default SectionTitle
+export default SectionTitle;
