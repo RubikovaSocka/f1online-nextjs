092f9bafa8233db57443fe201c1c6ff12f1b62b0
milanku <kubala@post.com>
2020-11-03 21:52:18 +0100

    Articleswith styled

--- components/ArticlesPanel/ArticlesPanel.js
+++ components/ArticlesPanel/ArticlesPanel.js
@@ -1,48 +1,100 @@
 import React from "react";
 import ArticlePreview from "../ArticlePreview";
-
-import styles from "./style.module.scss";
 import TrackedArtRePanel from "../Ads/TrackedArtRePanel";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  @media only screen and (min-width: 1024px) {
+    margin-top: 20px;
+    margin-bottom: initial;
+    height: initial;
+
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+
+  @media only screen and (min-width: 1280px) {
+    margin-top: 20px;
+  }
+
+  i {
+    width: 100%;
+
+    @media only screen and (min-width: 1024px) {
+      width: 198px;
+    }
+
+    @media only screen and (min-width: 1280px) {
+      width: 211px;
+    }
+  }
+`;
+
+const ArticleContainer = styled.div`
+  width: 100%;
+  margin-bottom: 5px;
+
+  @media only screen and (min-width: 1024px) {
+    width: 198px;
+    margin-bottom: 20px;
+  }
+
+  @media only screen and (min-width: 1280px) {
+    width: 211px;
+  }
+`;
 
 function ArticlesPanel({ posts }) {
   if (posts.length > 6) {
     return (
       <>
-        <div className={styles.container}>
+        <Container>
           {posts.slice(0, 6).map((post, index) => (
-            <div key={index} className={styles.item}>
+            <ArticleContainer key={index}>
               <ArticlePreview {...post} />
-            </div>
+            </ArticleContainer>
           ))}
           {/* Fill empty space (on right) in rows with less than 3 posts*/}
           <i aria-hidden="true"></i>
           <i aria-hidden="true"></i>
-        </div>
+        </Container>
         <div /*counter={counter}*/ style={{ width: "100%" }}>
           <TrackedArtRePanel changeable={true} report={true} />
         </div>
-        <div className={styles.container}>
+        <Container>
           {posts.slice(6, 12).map((post, index) => (
-            <div key={index} className={styles.item}>
+            <ArticleContainer key={index}>
               <ArticlePreview {...post} />
-            </div>
+            </ArticleContainer>
           ))}
           <i aria-hidden="true"></i>
           <i aria-hidden="true"></i>
-        </div>
+        </Container>
       </>
     );
   }
   return (
-    <div className={styles.container}>
+    <Container>
       {posts.map((post, index) => (
-        <div key={index} className={styles.item}>
+        <ArticleContainer key={index}>
           <ArticlePreview {...post} />
-        </div>
+        </ArticleContainer>
       ))}
       <i aria-hidden="true"></i>
       <i aria-hidden="true"></i>
-    </div>
+    </Container>
   );
 }
 
