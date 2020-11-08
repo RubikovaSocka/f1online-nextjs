81d00fdf074780477225d6ee06fa646f96c25e84
milanku <kubala@post.com>
2020-11-08 19:33:35 +0100

    social media to styled

--- components/SocialMediaPlugin/SocialMediaPlugin.js
+++ components/SocialMediaPlugin/SocialMediaPlugin.js
@@ -1,41 +1,80 @@
-import React from "react";
-import styles from "./style.module.scss";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: none;
+
+  @media only screen and (min-width: 1024px) {
+    height: 20px;
+    width: 170px;
+
+    display: flex;
+    flex-direction: row;
+
+    .item {
+      height: 18px;
+      margin: 0 2px;
+      transition: ease 0.2s;
+
+      img {
+        position: relative;
+        height: 15px;
+        margin: 0 2px;
+        margin-top: 3px;
+        display: inline-block;
+
+        &:hover {
+          cursor: pointer;
+          margin-top: -3px;
+        }
+
+        &.fbImg {
+          content: var(--fb-img-url);
+        }
+        &.instaImg {
+          content: var(--insta-img-url);
+        }
+        &.youtubeImg {
+          content: var(--youtube-img-url);
+        }
+      }
+    }
+  }
+`;
 
 function SocialMediaBasicPlugin() {
   return (
-    <div className={styles.container}>
-      <div className={styles.item}>
+    <Container>
+      <div className="item">
         <a
           className="noOutline"
           href="https://www.facebook.com/f1online.sk/"
           rel="noreferrer"
           target="_blank"
         >
-          <img alt="FB icon" className={styles.fbImg} />
+          <img alt="FB icon" className="fbImg" />
         </a>
       </div>
-
-      <div className={styles.item}>
+      <div className="item">
         <a
           href="https://www.instagram.com/stevoeiselef1/"
           rel="noreferrer"
           target="_blank"
           className="noOutline"
         >
-          <img alt="Instagram Icon" className={styles.instaImg} />
+          <img alt="Instagram Icon" className="instaImg" />
         </a>
       </div>
-      <div className={styles.item}>
+      <div className="item">
         <a
           href="https://www.youtube.com/channel/UCE54uS8jp-tlGC7wjUhnt4A"
           rel="noreferrer"
           target="_blank"
           className="noOutline"
         >
-          <img alt="Youtube Icon" className={styles.youtubeImg} />
+          <img alt="Youtube Icon" className="youtubeImg" />
         </a>
       </div>
-    </div>
+    </Container>
   );
 }
 
