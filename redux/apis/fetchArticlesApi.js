15c50070b27dea1e4bb195dec253745e6ca196d3
milanku <kubala@post.com>
2021-03-06 12:50:23 +0100

    updated articles api function, removed .flat()

--- redux/apis/fetchArticlesApi.js
+++ redux/apis/fetchArticlesApi.js
@@ -1,40 +1,33 @@
 import fetch from "isomorphic-fetch";
-import { result } from "lodash";
 import { URLS } from "./urls";
 
 const STICKY = "sticky=true&per_page=3";
 const NONSTICKY = "sticky=false&per_page=11";
 
-export default async function fetchArticles() {
-  console.log(
-    `${URLS.BASE}${URLS.ARTICLES_ENDPOINT}?${STICKY}&${URLS.fetchArticles}`,
-    `${URLS.BASE}${URLS.ARTICLES_ENDPOINT}?${NONSTICKY}&${URLS.fetchArticles}`
-  );
-  try {
-    return fetch(
-      `https://wpadmin.f1online.sk/wp-json/wp/v2/posts/?per_page=13`
-    ).then((res) => res.json());
+function flatten(array) {
+  if (array.length == 0) return array;
+  else if (Array.isArray(array[0]))
+    return flatten(array[0]).concat(flatten(array.slice(1)));
+  else return [array[0]].concat(flatten(array.slice(1)));
+}
 
+export default async function fetchArticles() {
+  try {
     return await Promise.all([
-      /*fetch(
-        `${URLS.BASE}${URLS.ARTICLES_ENDPOINT}?${STICKY}&${URLS.fetchArticles}`
+      fetch(
+        `${URLS.BASE}${URLS.ARTICLES_ENDPOINT}?${STICKY}&${URLS.previewFields}`
       ),
       fetch(
-        `${URLS.BASE}${URLS.ARTICLES_ENDPOINT}?${NONSTICKY}&${URLS.fetchArticles}`
-      ),*/
-      fetch(`https://wpadmin.f1online.sk/wp-json/wp/v2/posts/?per_page=13`),
+        `${URLS.BASE}${URLS.ARTICLES_ENDPOINT}?${NONSTICKY}&${URLS.previewFields}`
+      ),
     ])
-      /*.then((results) => Promise.all(results.map((r) => r.json())))
+      .then((results) => Promise.all(results.map((r) => r.json())))
       .then((results) => {
-        console.log(results);
-        return results.flat();
-      });*/
-      .then((results) => {
-        console.log(results.json());
-        results.json();
+        console.log(flatten(results));
+        return flatten(results);
       });
   } catch (e) {
-    //console.log("ERROR", e);
+    console.log("ERROR", e);
     throw new Error(e.response.data.Error);
   }
 }
